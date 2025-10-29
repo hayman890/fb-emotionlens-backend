6e775537779c8ad357a63b7b9de909d1b8ab91f8
Hay Man Htun <haymanhtun80@gmail.com>
2025-10-29 15:12:52 +0700

    backend: initial production setup

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = FastAPI()
+
+# Allow frontend requests (CORS)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# MongoDB connection
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client["social_media"]
+collection = db["fb_comments_sentiment"]
+
+@app.get("/")
+def root():
+    return {"message": "Facebook Sentiment API running!"}
+
+@app.get("/posts")
+def get_posts():
+    posts = list(collection.find({}, {"_id": 0}))
+    return posts
